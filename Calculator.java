fb130f6d8c488198ed08f882fd3d8412badde2db
Sanskar Patil <patil.sanskar.s9191@gmail.com>
2026-08-02 15:28:40 +0530

    A Calculator program using Conditionals

---
+++ Calculator.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Calculator{
+    public static void main(String[] args) {
+     Scanner in = new Scanner(System.in);
+     
+     // Taking the input till the user does not press X or x
+     int ans = 0;
+     while(true) {
+        
+        // Take the operator as an Input
+        System.out.println("Enter the Operator : ");
+        char op = in.next().trim().charAt(0);
+
+        if(op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+            // Input two numbers
+            System.out.println("Enter Two Numbers : ");
+            int num1 = in.nextInt();
+            int num2 = in.nextInt();
+
+            if(op == '+') {
+                ans = num1 + num2;
+            }
+            if(op == '-') {
+                ans = num1 - num2;
+            }
+            if(op == '*') {
+                ans = num1 * num2;
+            }
+            if(op == '/') {
+                if(num2 != 0) {
+                    ans = num1 / num2;
+                }
+            }
+            if(op == '%') {
+                ans = num1 % num2;
+            }
+        } else if(op == 'X' || op == 'x') {
+            break;
+        } else {
+            System.out.println("Invalid Operation");
+        }
+        System.out.println(ans);
+     }
+    }
+}
